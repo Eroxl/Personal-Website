55dfb870c47a37be2eded8c3a1cff20f84ceca3f
Erox1 <InfinityWaffles@outlook.com>
2021-11-10 15:07:08 -0800

    [Backend] Linted
    Auto linted the backend folder.

--- src/backend/.eslintrc.js
+++ src/backend/.eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: [['external', 'builtin'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
+    'import/extensions': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-        'import/order': [
-            'error',
-            {
-            groups: [['external', 'builtin'], 'internal', ['parent', 'sibling', 'index']],
-            'newlines-between': 'always',
-            },
-        ],
-        'import/extensions': 0,
-    },
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    }
+  },
 };
