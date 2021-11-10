ae4031ebb605101a85689f5a8e963f1efd48f9ca
Erox1 <InfinityWaffles@outlook.com>
2021-11-10 15:56:37 -0800

    [Backend] Added The Base For The Login File

---
+++ src/backend/src/accounts/login.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+
+router.post(
+  '/', 
+  async (res: express.Request, req: express.Response) => {
+    
+  }
+)
+
+export default router
--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 
+import login from './accounts/login';
+
 const app = express();
 const port = 8080;
 
 app.use(cors());
+
+app.use(
+  '/accounts/login/', 
+  login
+)
 
 // -=- Start The Express Server -=- 
 app.listen(port);
