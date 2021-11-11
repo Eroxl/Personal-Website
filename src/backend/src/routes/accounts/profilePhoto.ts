4574eeefb4351cd2410625cf07b3ac19e87c7b96
Erox1 <InfinityWaffles@outlook.com>
2021-11-10 20:25:09 -0800

    [Backend] Added Profile Photo's To The Backend

--- src/backend/src/routes/accounts/profilePhoto.ts
+++ src/backend/src/routes/accounts/profilePhoto.ts
@@ -1,11 +1,52 @@
 import express from 'express';
+import axios from 'axios';
+
+// -=- Models -=-
+import Users from '../../models/user';
 
 const router = express.Router();
 
 router.get(
   '/',
   async (req, res) => {
+    const { code } = req.query;
 
+    // -=- Check If The Code Exists -=-
+    if (code === undefined) {
+      // ~ 400 Bad Request
+      res.statusCode = 400;
+      return;
+    }
+
+    const user = await Users.findById(code, 'accessToken');
+    if (user === undefined) {
+      res.statusCode = 401;
+      return;
+    }
+
+    axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `token ${user.accessToken}`,
+      },
+    })
+      .then((userRequest) => {
+        const avatarURL = userRequest.data.avatar_url;
+
+        if (avatarURL === undefined) {
+          // ~ 404 Not Found
+          res.statusCode = 404;
+          return;
+        }
+
+        // ~ 200 Ok
+        res.statusCode = 200;
+        res.jsonp({
+          avatarURL,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 );
 
