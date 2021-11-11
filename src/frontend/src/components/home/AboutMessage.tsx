1430c6a4eb621474635363ab3b7a9cb690df8bb3
Erox1 <InfinityWaffles@outlook.com>
2021-11-11 10:12:53 -0800

    [Frontend] Added A About Portion To The Home Page
    Added an about page that explains a bit about the projects on this website as well as a bit about me.

---
+++ src/frontend/src/components/home/AboutMessage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+// TODO: Support Mobile
+// TODO: Convert This Whole Page Into A Custom Markdown Renderer
+const AboutMessage = () => (
+  <h1 className="font-bold">
+    <h1 className="text-6xl">About</h1>
+    <h1 className="text-3xl px-5">
+      <h1 className="text-2xl">
+        This is my personal website to show off some of the projects I&rsquo;m working on and to
+        test out ideas that I have
+        <br />
+        <br />
+        Speaking of testing out ideas while your exloring this page you should see some of them.
+        Heres a list of them with a bit of explanation about them.
+        <br />
+        <br />
+      </h1>
+      - Claiming Text
+      <br />
+      <h1 className="text-2xl px-5">
+        You&rsquo;ll notice small icons in the bottom left of most of the text on this page. If you
+        sign in with github those will switch to a small green + sign (or at least the ones that
+        haven&rsquo;t already been claimed) you can then press the plus button to add that text to
+        your account your profile photo will be placed besides the text for all users.
+      </h1>
+      <br />
+      - Trading Text
+      <br />
+      <h1 className="text-2xl px-5">
+        The large store icon on the topbar will take you to the
+        <a href="/marketplace" target="_blank" rel="noopener noreferrer" className="underline text-blue-400"> marketplace </a>
+        where you can trade portions of this website with other users.
+      </h1>
+      <br />
+      - Activity Monitor
+      <br />
+      <h1 className="text-2xl px-5">
+        If you scroll back up to the top of the page you&rsquo;ll see either
+        <span title="Editing A Python File" className="underline text-cyan-400"> &lt;programming/&gt; </span>
+        or
+        <span title="Just Sleeping" className="underline text-violet-400"> &lt;sleeping/&gt; </span>
+        if you hover over those they&rsquo;ll show a little description of my status gathered from a
+        <a href="https://github.com/Eroxl/Portfolio-Website" target="_blank" rel="noopener noreferrer" className="underline text-blue-400"> vscode plugin </a>
+        I coded
+        for instance &ldquo;Editing A Python File&rdquo; or &ldquo;Just Sleeping&rdquo;
+      </h1>
+    </h1>
+  </h1>
+);
+
+export default AboutMessage;
--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import HelloMessage from '../components/home/HelloMessage';
+import AboutMessage from '../components/home/AboutMessage';
+import Spacer from '../components/home/Spacer';
 
 const MainPage = () => (
-  <div className="bg-gray-100 h-full">
+  <div className="bg-gray-100 h-full px-10">
     <HelloMessage />
+    <Spacer />
+    <AboutMessage />
+    <Spacer />
   </div>
 );
 
