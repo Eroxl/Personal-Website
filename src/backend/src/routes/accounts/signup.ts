89263abb4068ff57d36939f7e3f61ee335532473
Erox1 <InfinityWaffles@outlook.com>
2021-11-10 19:24:42 -0800

    [Backend] Cleaned Signup File Up
    Cleaned the signup file up by removing if else statements and just returning after the if statement if it's true.

--- src/backend/src/routes/accounts/signup.ts
+++ src/backend/src/routes/accounts/signup.ts
@@ -6,8 +6,7 @@ import secrets from '../../secrets';
 
 const router = express.Router();
 
-// FIXME: Switch To Post For Production
-router.get(
+router.post(
   '/',
   async (req, res) => {
     const { code } = req.query;
@@ -16,36 +15,41 @@ router.get(
     if (code === undefined) {
       // ~ 400 Bad Request
       req.statusCode = 400;
-    } else {
-      // ?client_id=${secrets.githubClientID}&client_secret=${secrets.githubClientSecrets}&code=${}
-      // -=- Check If It's A Valid Github Code -=-
-      const accessTokenRequest = await axios.post('https://github.com/login/oauth/access_token', {
-        client_id: secrets.githubClientID,
-        client_secret: secrets.githubClientSecrets,
-        code,
-      }, {
-        headers: {
-          Accept: 'application/json',
-        },
-      });
-      const accessToken = accessTokenRequest.data.access_token;
-
-      // -=- Check If It's A Valid Access Token -=-
-      if (accessToken !== undefined) {
-        const user = new Users({
-          _id: code,
-          accessToken,
-          ownedItems: [],
-        });
-
-        user.save(() => {
-          res.statusCode = 409;
-        });
-        req.statusCode = 200;
-      } else {
-        req.statusCode = 401;
-      }
+      return;
     }
+
+    // -=- Get The Access Token -=-
+    const accessTokenRequest = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id: secrets.githubClientID,
+      client_secret: secrets.githubClientSecrets,
+      code,
+    }, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const accessToken = accessTokenRequest.data.access_token;
+
+    // -=- Check If It's A Valid Access Token -=-
+    if (accessToken === undefined) {
+      // ~ 401 Unauthorized
+      req.statusCode = 401;
+      return;
+    }
+
+    const user = new Users({
+      _id: code,
+      accessToken,
+      ownedItems: [],
+    });
+
+    // ~ 200 Ok
+    req.statusCode = 200;
+
+    user.save(() => {
+      // ~ 400 Conflict
+      res.statusCode = 409;
+    });
   },
 );
 
