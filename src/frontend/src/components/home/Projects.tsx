2ed0d3d84553bd863a4c97f49b213487cef852c0
Eroxl <InfinityWaffles@outlook.com>
2021-11-27 15:39:06 -0800

    [Frontend] Cleaned Up Project List Generation Code
    Cleaned up the project list generation code to instead use array.map to keep the script a bit cleaner and more compact.

--- src/frontend/src/components/home/Projects.tsx
+++ src/frontend/src/components/home/Projects.tsx
@@ -5,23 +5,23 @@ import ProjectsPortion from './ProjectsPortion';
 const ProjectsPage = (props: { projectsJSON: any[] }) => {
   const { projectsJSON } = props;
 
-  const projects: any[] = [];
-  projectsJSON.forEach((project: any) => {
-    projects.push(
-      <ProjectsPortion
-        name={project.name}
-        description={project.description}
-        imageURL={project.imageURL}
-        projectURL={project.homepage}
-      />,
-    );
-  });
-
   return (
     <div className="font-bold text-gray-800">
       <h1 className="sm:text-6xl text-5xl">Projects</h1>
       <div className="h-max flex gap-5 justify-evenly sm:justify-between flex-wrap mt-5 mx-5">
-        { projects }
+        {
+          Array.prototype.map(
+            (project: any, _) => (
+              <ProjectsPortion
+                name={project.name}
+                description={project.description}
+                imageURL={project.imageURL}
+                projectURL={project.homepage}
+              />
+            ),
+            projectsJSON,
+          )
+        }
       </div>
     </div>
   );
