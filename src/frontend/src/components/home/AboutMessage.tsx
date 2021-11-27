8efa8ca184613d4bd3b97582a375d6de2b7ec56e
Eroxl <InfinityWaffles@outlook.com>
2021-11-26 20:03:59 -0800

    [Frontend] Removed Activity Monitor

--- src/frontend/src/components/home/AboutMessage.tsx
+++ src/frontend/src/components/home/AboutMessage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// TODO: Convert This Whole Page Into A Custom Markdown Renderer
 const AboutMessage = () => (
   <h1 className="font-bold text-gray-800">
     <h1 className="sm:text-6xl text-5xl">About</h1>
@@ -10,8 +9,8 @@ const AboutMessage = () => (
         test out ideas that I have
         <br />
         <br />
-        Speaking of testing out ideas while your exloring this page you should see some of them.
-        Heres a list of them with a bit of explanation about them.
+        Speaking of testing out ideas while your exploring this page you should see some of them.
+        Here is a list of them with a bit of explanation about them.
         <br />
         <br />
       </h1>
@@ -21,20 +20,6 @@ const AboutMessage = () => (
         If you scroll down to the bottom while you&rsquo;re logged in you&rsquo;ll be able to sign
         this website by following the instructions at the bottom. This will add your profile picture
         to the footer forever.
-      </h1>
-      <br />
-      - Activity Monitor
-      <br />
-      <h1 className="sm:text-2xl text-xl sm:px-5 px-0 pl-5">
-        If you scroll back up to the top of the page you&rsquo;ll see either&ensp;
-        <a href="#top" title="Editing A Python File" className="underline text-cyan-400">&lt;programming/&gt;</a>
-        &ensp;or&ensp;
-        <a href="#top" title="Just Sleeping" className="underline text-violet-400">&lt;sleeping/&gt;</a>
-        &ensp;if you hover over those they&rsquo;ll show a little description of my status
-        &ensp;gathered from a&ensp;
-        <a href="https://github.com/Eroxl/Portfolio-Website" target="_blank" rel="noopener noreferrer" className="underline text-blue-400">vscode plugin</a>
-        &ensp;I coded for instance &ldquo;Editing A
-        Python File&rdquo; or &ldquo;Just Sleeping&rdquo;
       </h1>
     </h1>
   </h1>
--- src/frontend/src/components/home/HelloMessage.tsx
+++ src/frontend/src/components/home/HelloMessage.tsx
@@ -1,42 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Image from 'next/image';
 
-import { APIURL } from '../../constants/Constants';
 import downIcon from '../../public/svgs/down_icon.svg';
 
-const HelloMessage = () => {
-  const [isProgrammingState, setIsProgrammingState] = useState(true);
-  const [description, setDescription] = useState('');
-
-  useEffect(() => {
-    fetch(`${APIURL}status/`)
-      .then((status) => status.json())
-      .then((jsonStatus) => {
-        setDescription(jsonStatus.description);
-        setIsProgrammingState(jsonStatus.status === 'Programming');
-      })
-      .catch(() => {
-        setIsProgrammingState(false);
-        setDescription('Just Sleeping');
-      });
-  }, []);
-
-  return (
-    <div className="h-full">
-      <div className="px-10 table w-full h-screen absolute top-0 left-0">
-        <div className="table-cell font-bold text-3xl sm:text-4xl md:text-6xl text-center align-middle h-full text-gray-800">
-          <span>Hi, I&rsquo;m </span>
-          <a href="https://github.com/Eroxl" title="Github" target="_blank" rel="noopener noreferrer" className="underline text-green-400">{'<Eroxl/>'}</a>
-          <br />
-          <span>I&rsquo;m currently </span>
-          <a href="https://wakatime.com/@Eroxl" title={description} target="_blank" rel="noopener noreferrer" className={`underline ${isProgrammingState ? 'text-cyan-400' : 'text-violet-400'}`}>{isProgrammingState ? '<programming/>' : '<sleeping/>'}</a>
-          <a href="#aboutMessage" className="absolute bottom-0 left-1/2 block -ml-6">
-            <Image src={downIcon} height="48" width="48" className="animate-bounce" />
-          </a>
-        </div>
+const HelloMessage = () => (
+  <div className="h-full">
+    <div className="px-10 table w-full h-screen absolute top-0 left-0">
+      <div className="table-cell font-bold text-3xl sm:text-4xl md:text-6xl text-center align-middle h-full text-gray-800">
+        <span>Hi, I&rsquo;m </span>
+        <a href="https://github.com/Eroxl" title="Github" target="_blank" rel="noopener noreferrer" className="underline text-green-400">{'<Eroxl/>'}</a>
+        <br />
+        <span>I&rsquo;m currently </span>
+        <a href="https://wakatime.com/@Eroxl" title="Wakatime" target="_blank" rel="noopener noreferrer" className="underline text-cyan-400">{'<programming/>'}</a>
+        <a href="#aboutMessage" className="absolute bottom-0 left-1/2 block -ml-6">
+          <Image src={downIcon} height="48" width="48" className="animate-bounce" />
+        </a>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default HelloMessage;
