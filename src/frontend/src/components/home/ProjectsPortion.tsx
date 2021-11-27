c16ca38f0ed22ce3fbcb84e9f4a451d93e3d56fb
Eroxl <InfinityWaffles@outlook.com>
2021-11-26 20:09:03 -0800

    [Frontend] Updated Style Of Projects Portion

--- src/frontend/src/components/home/ProjectsPortion.tsx
+++ src/frontend/src/components/home/ProjectsPortion.tsx
@@ -19,14 +19,15 @@ const ProjectsPortion = (props: Props) => {
   return (
     <a
       href={projectURL}
-      className="w-full lg:w-5/12 h-auto bg-gray-800 rounded sm:p-5 p-3 text-gray-50 hover:animate-projects"
+      className="w-full lg:w-5/12 h-auto bg-gray-800 rounded sm:p-5 p-3 text-gray-50 hover:animate-projects group"
+      title="Open Website In New Tab"
       target="_blank"
       rel="noopener noreferrer"
     >
       <div className="pb-2s">
-        <Image src={imageURL} width="1280" height="640" className="rounded-sm object-cover" />
+        <Image src={imageURL} width="1280" height="640" className="rounded-sm object-cover " />
       </div>
-      <h1 className="text-xl">{name.replace(new RegExp('-', 'g'), ' ')}</h1>
+      <h1 className="text-xl group-hover:underline">{name.replace(new RegExp('-', 'g'), ' ')}</h1>
       <h1 className="text-sm">{description}</h1>
     </a>
   );
