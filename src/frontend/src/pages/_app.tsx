84b554e421a4a71e0c6a43b9aefa9e7b3b855577
Eroxl <InfinityWaffles@outlook.com>
2021-12-01 23:38:50 -0800

    [Frontend] Added Meta Tags

--- src/frontend/src/pages/_app.tsx
+++ src/frontend/src/pages/_app.tsx
@@ -13,6 +13,21 @@ function MainApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>Eroxl&rsquo;s Personal Website</title>
+        {/* Meta Tags */}
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="description" content="Eroxl's personal website for testing out ideas and sharing projects they are working on." />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Eroxl's Personal Website" />
+        <meta property="og:description" content="My personal website for testing out ideas and sharing projects im working on" />
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/logo.png" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:title" content="Eroxl's Personal Website" />
+        <meta name="twitter:description" content="My personal website for testing out ideas and sharing projects im working on" />
+        <meta name="twitter:image" content="/logo.png" />
       </Head>
       <NavigationBar />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
