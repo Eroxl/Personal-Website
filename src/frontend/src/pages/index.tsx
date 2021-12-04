3d013f3fc08f14474bc1157c1b09e348cc153cc4
Eroxl <InfinityWaffles@outlook.com>
2021-12-03 17:06:00 -0800

    [Frontend] Switched To Static Site Generation

--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import HelloMessage from '../components/home/HelloMessage';
 import AboutMessage from '../components/home/AboutMessage';
@@ -8,29 +8,35 @@ import ProjectsPage from '../components/home/Projects';
 import Spacer from '../components/home/Spacer';
 import { APIURL } from '../constants/Constants';
 
-const MainPage = (props: { projectsJSON: any[] }) => {
-  const { projectsJSON } = props;
+const MainPage = (
+  props: { projectsJSON: any[],
+  // eslint-disable-next-line camelcase
+  codeTimeJSON: {data: {total_seconds_including_other_language: string}} },
+) => {
+  const { projectsJSON, codeTimeJSON } = props;
   return (
     <div className="bg-gray-100 h-full px-10">
-      <HelloMessage />
+      <HelloMessage codeTimeJSON={codeTimeJSON} />
       <Spacer id="aboutMessage" />
       <AboutMessage />
       <Spacer id="projects" />
       <ProjectsPage projectsJSON={projectsJSON} />
       <Spacer id="signing" />
-      {/* <SigningPage />
-      <Spacer /> */}
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const projectsData = await fetch(`${APIURL}projects`);
   const projectsJSON = await projectsData.json();
+
+  const codeTimeData = await fetch('https://wakatime.com/api/v1/users/Eroxl/stats/');
+  const codeTimeJSON = await codeTimeData.json();
 
   return {
     props: {
       projectsJSON,
+      codeTimeJSON,
     },
   };
 };
