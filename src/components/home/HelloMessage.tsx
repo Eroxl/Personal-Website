ebc5cd5af03bc19a6b2eae7aa910e8b28ce682ac
Eroxl <InfinityWaffles@outlook.com>
2021-12-30 13:54:28 -0500

    Updated Welcome Message To Work In Dark Mode

--- src/components/home/HelloMessage.tsx
+++ src/components/home/HelloMessage.tsx
@@ -8,7 +8,7 @@ const HelloMessage = (
   return (
     <div className="h-full">
       <div className="px-10 table w-full h-screen absolute top-0 left-0">
-        <div className="table-cell font-bold text-3xl sm:text-4xl md:text-6xl text-center align-middle h-full text-gray-800">
+        <div className="table-cell font-bold text-3xl sm:text-4xl md:text-6xl text-center align-middle h-full text-gray-800 dark:text-gray-50">
           <span>Hi, I&rsquo;m </span>
           <a href="https://github.com/Eroxl" title="Github" target="_blank" rel="noopener noreferrer" className="underline text-green-400">Eroxl</a>
           <br />
@@ -22,7 +22,7 @@ const HelloMessage = (
               height="24px" 
               viewBox="0 0 24 24" 
               width="24px"
-              className="animate-bounce w-12 h-12 fill-current text-gray-800"
+              className="animate-bounce w-12 h-12 fill-current text-gray-800 dark:text-gray-50"
             ><g><rect fill="none" height="24" width="24"/></g><g><g><polygon points="18,6.41 16.59,5 12,9.58 7.41,5 6,6.41 12,12.41"/><polygon points="18,13 16.59,11.59 12,16.17 7.41,11.59 6,13 12,19"/></g></g></svg>
           </a>
         </div>
