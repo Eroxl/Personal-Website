918288bee559a089649862a6ae0175e2fd4a459f
Eroxl <InfinityWaffles@outlook.com>
2021-12-30 13:46:00 -0500

    Updated About Message

--- src/components/home/AboutMessage.tsx
+++ src/components/home/AboutMessage.tsx
@@ -4,11 +4,15 @@ const AboutMessage = () => (
   <h1 className="font-bold text-gray-800">
     <h1 className="sm:text-6xl text-5xl">About</h1>
     <h1 className="sm:text-3xl text-2xl sm:px-5 px-0 pl-5">
-      <h1 className="sm:text-2xl text-xl">
+      <p className="sm:text-2xl text-xl">
         This is my personal website to show off some of the projects I&rsquo;m working on and to
         test out ideas that I have!
         <br />
-      </h1>
+        <br />
+        This site was created using NextJS and Typescript you can see the source code{' '}
+        <a className="underline text-cyan-400" href="https://github.com/Eroxl/Personal-Website">here</a>.
+        <br />
+      </p>
     </h1>
   </h1>
 );
