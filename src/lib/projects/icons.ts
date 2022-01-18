747a4e958740966bff77ab48bf933163c6583aa6
Eroxl <InfinityWaffles@outlook.com>
2022-01-17 20:00:05 -0800

    Removed Some Projects

--- src/lib/projects/icons.ts
+++ src/lib/projects/icons.ts
@@ -1,8 +1,4 @@
 const icons: { [key: string]: string[] } = {
-  // ~ https://github.com/Eroxl/MacOS-Folder-Icon-Changer
-  425398123: [
-    'https://repository-images.githubusercontent.com/425398123/e48521cb-eab3-4625-bd0d-49b1e64f0283'
-  ],
   // ~ https://github.com/Eroxl/Personal-Website
   425653836: [
     'https://repository-images.githubusercontent.com/425653836/781f96f2-4a25-4eb9-86df-93376136428a'
@@ -10,7 +6,6 @@ const icons: { [key: string]: string[] } = {
   // ~ https://github.com/BC-Open-Legislature/Legislature-Site-Frontend
   418293606: [
     "https://raw.githubusercontent.com/BC-Open-Legislature/Legislature-Site-Frontend/main/images/Screenshots/Debates%20Page%20Updated.png",
-    "https://raw.githubusercontent.com/BC-Open-Legislature/Legislature-Site-Frontend/main/images/Screenshots/Members%20Page.png",
   ],
   // ~ https://github.com/LiveTL/LiveTL/
   314984533: [
