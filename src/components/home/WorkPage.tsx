7ab1f14f96144d1b12e828a5ceaae74454351f13
Evan <infinitywaffles@outlook.com>
2025-03-02 19:13:22 -0800

    Update WorkPage.tsx

--- src/components/home/WorkPage.tsx
+++ src/components/home/WorkPage.tsx
@@ -7,7 +7,7 @@ const WorkPage = () => (
   <h1 className="sm:text-3xl text-2xl sm:px-5 px-0 pl-5">
     <span className="sm:text-2xl text-xl">
       There&rsquo;s nothing to see here yet but you can change that! Check out my{' '}
-      <a className="underline text-cyan-400" href="https://eroxl.notion.site/Resume-17629dba988d460d9f7463c28672a4e2">resume</a>
+      <a className="underline text-cyan-400" href="./resume.pdf">resume</a>
       .
       <br />
     </span>
